6d1d874f640c2e160dab4dd402662e59fb5869b0
medszilla26 <pablohdzjr@gmail.com>
2020-09-12 19:23:41 -0500

    installed missing packages

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,1 +1,13 @@
-export { default } from "./Nav";
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        Google Book Search
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDom from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDom.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
